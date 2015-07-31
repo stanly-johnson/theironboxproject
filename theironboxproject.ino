294b767d1854f9d174489cb1e18f163fc4e6f20c
Stanly Johnson <stanlyjohnson@outlook.com>
2015-07-31 22:43:08 +0530

    theironboxproject

---
+++ theironboxproject.ino
@@ -0,0 +1,42 @@
+/* Code written for the IronBox project */
+
+int relay_pin = 8;
+int signal_pin =5;
+int status_pin = 0;
+unsigned long time;
+
+void setup()
+{
+  pinMode(relay_pin,OUTPUT);
+  pinMode(signal_pin,INPUT);
+  pinMode(status_pin,INPUT);
+  digitalWrite(relay_pin,LOW);
+  digitalWrite(signal_pin,LOW);
+  digitalWrite(status_pin,LOW);
+}
+
+void loop()
+{
+  while(status_pin==HIGH)
+  {
+    time=millis();
+    digitalWrite(relay_pin,HIGH);
+    
+    if(signal_pin==HIGH)
+     {
+       if(time>30000)
+       {
+         digitalWrite(relay_pin,LOW);
+       }
+     }
+     
+     else{
+       if(time>3000)
+       {
+         digitalWrite(relay_pin,LOW);
+       }
+     }
+  }
+    
+  
+}
